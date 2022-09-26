eb8440ad792082e2df09e58e83678c5843b99864
ziyodbek0300 <mr.prgrmr03@gmail.com>
2022-09-27 00:58:09 +0500

    pptx viewer fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React, {useEffect, useRef, useState} from 'react';
-import FileViewer from 'react-file-viewer';
-import {CustomErrorComponent} from 'custom-error';
+import React, { useState } from 'react';
 import fileImg from "./QoraQo'rg'on.png";
 import filePDF from "./qwerty.pdf";
-import file from "./Extlst-test.pptx";
 import RViewerJS from 'viewerjs-react'
 import 'viewerjs-react/dist/index.css'
 
+// import FileViewer from 'react-file-viewer';
+// import file from "./Extlst-test.pptx";
 // const type = 'png'
-const type = 'pdf'
+// const type = 'pdf'
+// const type = 'pptx'
 
 function App() {
-    const [view, setView] = useState(false);
-    const [page, setPage] = useState(15);
-    const numRef = useRef(null);
+    const [page] = useState(15);
 
     return (<div>
-        <div id={"first"} style={{padding: '10px'}}>
-            <button onClick={() => setView(!view)} style={{margin: '10px'}}>Show</button>
-            {view && <div style={{width: '50%', margin: '0 auto'}}>
-                <FileViewer
-                    fileType={type}
-                    filePath={file}
-                    errorComponent={CustomErrorComponent}
-                    onError={() => console.log()}/>
-            </div>}
+        <div id={"second"} style={{ padding: '30px 0' }}>
+            <h1>PPTX Viewer</h1>
+            <iframe
+                title={'PDF-Viewer'}
+                src={`https://view.officeapps.live.com/op/embed.aspx?src=https://scholar.harvard.edu/files/torman_personal/files/samplepptx.pptx`}
+                frameBorder={0}
+                style={{ height: '100vh', width: '90vw' }}></iframe>
         </div>
-        <div id={"second"}>
-            <div style={{margin: '100px'}}>
-                <input type="number" ref={numRef}/>
-                <button onClick={() => setPage(numRef.current.value)}>Hello</button>
-            </div>
-            <iframe src={`${file}#page=${page}`} frameBorder={0} style={{height: '100vh', width: '90vw'}}></iframe>
+
+        <div id={"second"} style={{ padding: '30px 0' }}>
+            <h1>PDF Viewer with page</h1>
+            <iframe
+                title={'PPTX-Viewer'}
+                src={`${filePDF}#page=${page}`}
+                frameBorder={0}
+                style={{ height: '100vh', width: '90vw' }}></iframe>
         </div>
-        <div id={"third"}>
+
+        <div id={"third"} style={{ padding: '30px 0' }}>
+            <h1>Third way</h1>
             <RViewerJS>
-                <img src={fileImg} alt={"sakjdf"}/>
+                <img src={fileImg} alt={"sakjdf"} />
             </RViewerJS>
         </div>
     </div>);
